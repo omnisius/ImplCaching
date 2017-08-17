699795578a376c6583d36ebd251c8db805b6eb0c
omnisius <yfabufgjgeyfhvjym>
2017-08-17 18:24:57 +0300

    Person table created and filled with FlyWay

---
+++ src/main/resources/db/migration/V2__inserting_persons.sql
@@ -0,0 +1,10 @@
+INSERT INTO Person
+VALUES (1, 'Darth Revan'),
+       (2, 'Darth Seon'),
+       (3, 'Darth Bane'),
+       (4, 'Darth Malak'),
+       (5, 'Darth Andeddu'),
+       (6, 'Darth Ruin'),
+       (7, 'Darth Sidious'),
+       (8, 'Darth Kreya'),
+       (9, 'Darth Vader');
