86fd1e4a73c219b78d0600db257c63912ccf3fb7
omnisius <yfabufgjgeyfhvjym>
2017-08-18 18:04:36 +0300

    small changes in performance test

--- src/test/java/PerformanceTest.java
+++ src/test/java/PerformanceTest.java
@@ -14,13 +14,13 @@ public class PerformanceTest {
         dao.getPersons();
         long endTime = System.currentTimeMillis( );
         long time = endTime - startTime;
-        System.out.println(time);
+        System.out.println("Time of retrieving data from database is " + time + " ms");
 
         long startTimeAfterCache = System.currentTimeMillis( );
         dao.getPersons();
         long endTimeAfterCache = System.currentTimeMillis( );
         long timeAfterCache = endTimeAfterCache - startTimeAfterCache;
-        System.out.println(timeAfterCache);
+        System.out.println("Time of getting data from cache is " + timeAfterCache + " ms");
         assertTrue( "Is time after data has been loaded to cache is ten times greater that before",time / 10 > timeAfterCache);
     }
 }
